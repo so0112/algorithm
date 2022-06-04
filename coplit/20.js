c4ccf4abe6540c10d97d0834517031e7e5a4aff2
so0112 <tkddh0112@gmail.com>
2022-06-04 15:32:34 +0900

    연속되는 문자열 압축

---
+++ coplit/20.js
@@ -0,0 +1,34 @@
+// before 변수에 str[i - 1] 값 할당
+// cnt = 1 할당 이후 나오는 문자열도 같을 경우 cnt++ => ex) 'ww' 일 경우 2개 중복이기 때문에 cnt 2되도록 만들어 줘야함
+// 다른 문자열이 나오는 경우에 cnt + 중복문자열 반환하도록
+
+// str의 마지막문자열은 비교할 대상이 없기 때문에 문자열에 공백을 하나 추가
+
+function compressString(str) {
+    // TODO: 여기에 코드를 작성합니다.
+  
+    let before = str[0]
+    let cnt = 1;
+    let result = ''
+    str += ' '
+  
+    for (let i = 1; i < str.length; i++){
+      
+      if(before == str[i]){
+        cnt++;
+      } else {
+        if(cnt >= 3){
+          result += cnt + before
+        }else{
+          result += before.repeat(cnt)
+        }
+  
+      before = str[i]
+      cnt = 1;
+      }
+    }
+    return result
+  }
+  
+  let output = compressString('dsafdasfffffsgaaaaabbbscccdd');
+  console.log(output)
