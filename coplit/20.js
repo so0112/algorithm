09b221af50dbafbab5cbf3e3055f11928e58ee5d
so0112 <tkddh0112@gmail.com>
2022-06-04 15:42:39 +0900

    length 변경

--- coplit/20.js
+++ coplit/20.js
@@ -2,33 +2,32 @@
 // cnt = 1 할당 이후 나오는 문자열도 같을 경우 cnt++ => ex) 'ww' 일 경우 2개 중복이기 때문에 cnt 2되도록 만들어 줘야함
 // 다른 문자열이 나오는 경우에 cnt + 중복문자열 반환하도록
 
-// str의 마지막문자열은 비교할 대상이 없기 때문에 문자열에 공백을 하나 추가
+// str의 마지막문자열은 비교할 대상이 없기 때문에 i <= str.length로 마지막 문자열 비교가능하도록 설정
 
 function compressString(str) {
     // TODO: 여기에 코드를 작성합니다.
   
     let before = str[0]
     let cnt = 1;
-    let result = ''
-    str += ' '
+    let result = '';
   
-    for (let i = 1; i < str.length; i++){
+    for (let i = 1; i <= str.length; i++){
       
       if(before == str[i]){
         cnt++;
       } else {
         if(cnt >= 3){
-          result += cnt + before
+          result += cnt + before;
         }else{
-          result += before.repeat(cnt)
+          result += before.repeat(cnt);
         }
   
-      before = str[i]
+      before = str[i];
       cnt = 1;
       }
     }
-    return result
+    return result;
   }
   
   let output = compressString('dsafdasfffffsgaaaaabbbscccdd');
-  console.log(output)
+  console.log(output);
