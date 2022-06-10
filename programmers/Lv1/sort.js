231e5ddad0566942d2bbd41037f49b0b54ed6d47
so0112 <tkddh0112@gmail.com>
2022-06-10 21:04:03 +0900

    문자열 내 마음대로 정렬하기

---
+++ programmers/Lv1/sort.js
@@ -0,0 +1,17 @@
+function solution(strings, n) {
+    
+    strings.sort((a, b) => {
+    if (a[n] < b[n]) {
+        return -1;
+    }
+    if (a[n] > b[n]) {
+        return 1;
+    }
+    if (a[n] === b[n]) {
+        return a < b ? -1 : 1;
+    }
+    return 0;
+  });
+    
+    return strings;
+}
