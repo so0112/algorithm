de518e06ac59f741dc2ba1d20e22ef5b902fb065
so0112 <tkddh0112@gmail.com>
2022-06-13 23:11:28 +0900

    좀 더 깔끔한 코드

--- coplit/section2/14.js
+++ coplit/section2/14.js
@@ -1,22 +1,14 @@
-// 0부터 (i - 1)번째 요소까지 담는 새로운 배열 선언 === slice(0, i)
-// 위 조건을 만족하는 배열.reduce((acc, cur) => return acc + cur)
-
-
 function superIncreasing(arr) {
     // TODO: 여기에 코드를 작성합니다.
-    
+    let sum = 0;
   
-    for (let i = 1; i < arr.length; i++){
-      let sliceArr = arr.slice(0, i) 
-      let sumBefore= sliceArr.reduce((acc,cur) => {return acc + cur}, 0)
-      console.log(sumBefore)
-      if(sumBefore >= arr[i]){
+    for (let i = 0; i < arr.length - 1; i++){
+      sum += arr[i]
+      if (sum >= arr[i + 1]){
         return false;
       }
     }
     return true;
-  }
-
-  let output = superIncreasing([979, 1737, -2146, -337, 1316]);
-console.log(output); // --> true
+}
+  
   
