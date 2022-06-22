b7c5b7e3a5656be9f5d30813b5177e8a21df6715
so0112 <tkddh0112@gmail.com>
2022-06-22 09:21:56 +0900

    중복 문자열 압축

--- coplit/section2/20.js
+++ coplit/section2/20.js
@@ -1,5 +1,5 @@
-// before 변수에 str[i - 1] 값 할당
-// cnt = 1 할당 이후 나오는 문자열도 같을 경우 cnt++ => ex) 'ww' 일 경우 2개 중복이기 때문에 cnt 2되도록 만들어 줘야함
+// before 변수에 str[i - 1] 값 할당 -> 반복문 마지막에 before 변수에 str[i] 값을 할당하고 i++ 
+// cnt = 1 -> 다음 문자열이 같을 경우 cnt++ => ex) 'ww' 일 경우 2개 중복 따라서 cnt 2되도록 만들어 줘야함
 // 다른 문자열이 나오는 경우에 cnt + 중복문자열 반환하도록
 
 // str의 마지막문자열은 비교할 대상이 없기 때문에 i <= str.length로 마지막 문자열 비교가능하도록 설정
@@ -9,7 +9,7 @@ function compressString(str) {
 
   let before = str[0];
   let cnt = 1;
-  let result = "";
+  let result = '';
 
   for (let i = 1; i <= str.length; i++) {
     if (before == str[i]) {
